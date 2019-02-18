fa634f2a4b247fde813ee10de0ae5b15be151b18
UmutA <umut.acikgoz@gmail.com>
2019-02-19 01:22:37 +0300

    Create core.php
    script

---
+++ core.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(E_PARSE & E_WARNING);
+header("Content-Type: application/json; charset=UTF-8");
+include "pdo.php";
+
+$db = new DB();
+$tbl = 'kurum';
+$dta = $db->getRows($tbl, array('where'=>array('ilid'=>'9'), 'order_by'=>'id DESC'));
+
+foreach ($dta as $v) {
+
+  	foreach ($v as $k1 => $v1) {
+  		$v1 = mb_convert_encoding($v1, "UTF-8", "auto");
+  		$a[] = "\"".$k1 ."\":\"". $v1 ."\"";
+  	}
+  	$sr .= "{". implode(',', array_values($a)) ."},";
+
+}
+$sr = rtrim($sr, ",");
+// json
+echo "[".$sr."]";
+
+?>
